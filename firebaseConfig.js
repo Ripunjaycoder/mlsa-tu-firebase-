3e9737e161e9828fe4fb42f511de6065b441104f
Ripunjay Deka <92496386+Ripunjaycoder@users.noreply.github.com>
2024-10-28 14:29:54 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,7 +4,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCNh_DsOHo0Xx66Bqur_Rp-vguLzyOEAAU",
   authDomain: "mlsa-tu.firebaseapp.com",
   projectId: "mlsa-tu",
   storageBucket: "mlsa-tu.appspot.com",
