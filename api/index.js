3cfc2770702d10723539d312a3064324f9e9f43c
Ripunjay Deka <92496386+Ripunjaycoder@users.noreply.github.com>
2024-10-28 15:09:46 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -64,7 +64,5 @@ app.post('/api/submit-form', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
+
